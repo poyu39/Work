c90b24373c9ecee2c8889d20373e3a9eca0aaaa8
Poyu <42506064+poyu39@users.noreply.github.com>
2022-09-20 08:51:20 +0800

    Build class name

---
+++ Cpp/IECS1006/20220920/D1009212_Q1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    printf("welcome to C\n");
+
+    // system("pause");
+    system("read -n 1 -s -p 'Press any key to continue...'; echo");
+    // pause();
+    return 0;
+}
