99ab5ac482795ca66accfad9a45a77d952fcc10c
poyu39 <bruce920309@gmail.com>
2022-09-20 10:10:15 +0800

    20220920

--- Cpp/IECS1006/20220920/D1009212_Q1.cpp
+++ Cpp/IECS1006/20220920/D1009212_Q1.cpp
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-int main()
-{
-    printf("welcome to C\n");
+int main(){
+    int a, b, c;
+    int y1, y2, y3, y4;
+    printf("input a b c\n");
+    scanf("%d %d %d", &a, &b, &c);
+    y1 = a + b + c;
+    printf("%d\n", y1);
 
-    // system("pause");
-    system("read -n 1 -s -p 'Press any key to continue...'; echo");
-    // pause();
+    // scanf("%d %d %d", &a, &b, &c);
+    y2 = a - b - c;
+    printf("%d\n", y2);
+
+    // scanf("%d %d %d", &a, &b, &c);
+    y3 = a * b * c;
+    printf("%d\n", y3);
+
+    // scanf("%d %d %d", &a, &b, &c);
+    y4 = a / b / c;
+    printf("%d\n", y4);
+
+    system("pause");
     return 0;
 }
